97e5c977c27538ff7aebf4e9e4874e83659be6a4
jlz <jlz@qq.com>
2020-04-23 17:54:57 +0800

    gitupdate

--- finutils/time_range.py
+++ finutils/time_range.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from datetime import datetime, time, timedelta
+from collections import Iterable
 
 class TimeRange:
     def __init__(self, start: time, end:time):
@@ -8,7 +9,11 @@ class TimeRange:
         self.end = end
 
     def __contains__(self, x:time):
-        return x>=self.start and x<=self.end
+        def contains(x):
+            return x>=self.start and x<=self.end
+        if isinstance(x, Iterable):
+            return [contains(i) for i in x]
+        return contains(x)
 
     def __repr__(self):
         return f'TimeRange {self.start} -> {self.end}'
@@ -25,12 +30,16 @@ class MultiTimeRange:
     def __init__(self, *args):
         self.time_range_list = args
 
-    def __contains__(self, x:time):
-        for time_range in self.time_range_list:
-            if x in time_range:
-                return True
-        return False
-    
+    def __contains__(self, x):
+        def contains(x):
+            for time_range in self.time_range_list:
+                if x in time_range:
+                    return True
+            return False
+        if isinstance(x, Iterable):
+            return [contains(i) for i in x]
+        return contains(x)
+            
     def __repr__(self):
         s = 'MultiTimeRange:\n'
         for tr in self.time_range_list:
